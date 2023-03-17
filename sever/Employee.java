3cde3e25786d0c7e93e6f58232f0d14ea1c3cbe5
Lesiba1223 <127945306+Lesiba1223@users.noreply.github.com>
2023-03-17 11:35:38 +0000

    Updated employee class
    Updated employee class

--- sever/Employee.java
+++ sever/Employee.java
@@ -1,28 +1,70 @@
-class Employee {
-    private String Name;
-    private String Surname;
-    private String Email;
-    
-    public Employee(String _Name, String _Surname, String _Email)
-    {
-        Name = _Name;
-        Surname = _Surname;
-        Email = _Email;
-    }
-    public String GetName()
-    {
-        return Name;
-    }
-    public String GetSurName()
-    {
-        return Surname;
-    }
-    public String GetEmail()
-    {
-        return Email;
-    }
-    public String toSTring()
-    {
-        return Name + " " + Surname + " " + Email;
-    }
+package com.sjprogramming.springmysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+@Table
+public class Employee {
+	//auto generate employee ID(Primary key)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+	@Column
+	private String Name;
+	@Column
+	private String Surname;
+	@Column
+	private String Email;
+	
+	//constructor
+	public Employee(){}
+	
+	//Constructor
+	public Employee(int id, String name, String surname, String email) {
+		super();
+		Id = id;
+		Name = name;
+		Surname = surname;
+		Email = email;
+	}
+
+	//Getters and setters
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getSurname() {
+		return Surname;
+	}
+
+	public void setSurname(String surname) {
+		Surname = surname;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	
 }
